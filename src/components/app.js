a5dd092eec302850c157083e0e70c5d06770505a
Minh Pham <mpham@naver.com>
2019-03-04 12:54:36 +0100

    Add search and api call functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,32 @@
 import React from "react";
+import axios from "axios";
 import "../styles/app.css";
 import SearchBar from "./search_bar";
 
+const API_KEY = process.env.REACT_APP_UNSPLASH_API;
+
 class App extends React.Component {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${API_KEY}`
+      }
+    });
+    this.setState({
+      images: response.data.results
+    });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui-container">
+        <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        {this.state.images.length} images
       </div>
     );
   }
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,20 +2,32 @@ import React from "react";
 import "../styles/search_bar.css";
 
 class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
   onInputChange = event => {
-    console.log(event.target.value);
+    this.setState({
+      term: event.target.value
+    });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSearchSubmit(this.state.term);
   };
 
   render() {
     return (
-      <div>
-        <h1>Unsplash Images</h1>
-        <form className="ui form">
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
+            <label htmlFor="search">Unsplash Images</label>
             <input
               type="text"
               name="search"
               placeholder="Search..."
+              value={this.state.term}
               className="search-input"
               onChange={this.onInputChange}
             />
